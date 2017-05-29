408dc6ca56845125fcf18b80dc2da3920137c536
risatrix <amandakrauss@Amanda-Krausss-iMac-2.local>
2017-05-29 16:02:28 -0500

    try scheduler for timing

--- owlbot.py
+++ owlbot.py
@@ -1,52 +1,60 @@
 from config import *
 
 import json
+
 import nltk
 from nltk.tokenize.moses import MosesDetokenizer
 
-import tweepy
 
 import requests
+import schedule
+import time
+import tweepy
 
+def get_owl():
+    # the base url to get the sortes text
+    url = "http://api.aeneid.eu/sortes"
 
+    r = requests.get(url)
 
-# the base url to get the sortes text
-url = "http://api.aeneid.eu/sortes"
+    doc = r.json()
+    raw_text = doc['text'][0]
 
-r = requests.get(url)
+    # break the sentence down into words, then parts of speech
+    text = nltk.word_tokenize(raw_text)
+    tokenized_list = nltk.pos_tag(text)
 
-doc = r.json()
-raw_text = doc['text'][0]
+    # find a word the first noun so we can replace it
+    replacement_index = ''
+    for index, token in enumerate(tokenized_list):
+        if token[1][:1] == 'N':
+            replacement_index = index
+            break
 
-# break the sentence down into words, then parts of speech
-text = nltk.word_tokenize(raw_text)
-tokenized_list = nltk.pos_tag(text)
+    #  replace that word with 'owl'
+    text[replacement_index] = 'owl'
 
-# find a word the first noun so we can replace it
-replacement_index = ''
-for index, token in enumerate(tokenized_list):
-    if token[1][:1] == 'N':
-        replacement_index = index
-        break
+    # put the sentence back together again
+    detokenizer = MosesDetokenizer()
+    sentence = detokenizer.detokenize(text, return_str=True)
+    print (sentence)
 
-#  replace that word with 'owl'
-text[replacement_index] = 'owl'
+    # # get Twitter acess
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+    except:
+        print("Error: Authentication failed")
 
-# put the sentence back together again
-detokenizer = MosesDetokenizer()
-sentence = detokenizer.detokenize(text, return_str=True)
-print (sentence)
+    # tweet the owl sentence
+    api.update_status(sentence)
 
-# # get Twitter acess
-try:
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-except:
-    print("Error: Authentication failed")
+schedule.every().day.at("08:30").do(get_owl)
 
-# tweet the owl sentence
-api.update_status(sentence)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 # add some emoji for fun
 #test owl
