92ef67ed0666356d6a6c6992afbfe609687dd8a3
risatrix <amandakrauss@Amanda-Krausss-iMac-2.local>
2017-05-29 09:31:45 -0500

    add initial bot script

---
+++ owlbot.py
@@ -0,0 +1,44 @@
+import requests
+import nltk
+import json
+
+from nltk.tokenize.moses import MosesDetokenizer
+
+# nltk.download('punkt')
+# nltk.download('maxent_treebank_pos_tagger')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('perluniprops')
+
+# the base url to get the sortes text
+url = "http://api.aeneid.eu/sortes"
+
+r = requests.get(url)
+
+doc = r.json()
+raw_text = doc['text'][0]
+
+# break the sentence down into words, then parts of speech
+text = nltk.word_tokenize(raw_text)
+tokenized_list = nltk.pos_tag(text)
+
+# find a word the first noun so we can replace it
+replacement_index = ''
+for index, token in enumerate(tokenized_list):
+    if token[1][:1] == 'N':
+        replacement_index = index
+        break
+
+#  replace that word with 'owl'
+text[replacement_index] = 'owl'
+
+# put the sentence back together again
+detokenizer = MosesDetokenizer()
+sentence = detokenizer.detokenize(text, return_str=True)
+print (sentence)
+
+# add some emoji for fun
+#test owl
+print(u'\U0001F989')
+#test chili peppers
+print(u"\U0001F336")
+print(u"\U000131B2")
